50e9e6542c76f1a235591cecd4a7aebda81da648
Brian Archibald <brianallenarchibald@gmail.com>
2020-10-05 16:23:22 -0500

    Refactor: Added lambda function example

---
+++ lambda.py
@@ -0,0 +1,11 @@
+# regular function
+def multiply_by_two(x):
+    return x * 2
+
+# Now using a lambda function
+multiply_by_two = lambda x: x * 2
+multiply_by_two(2)
+
+# 4
+
+# Lambdas can be anonymous functions and do not have to be named
